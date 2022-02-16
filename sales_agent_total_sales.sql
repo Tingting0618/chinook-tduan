3d2d7f258e3646ce199b8bb08fed76014a31d3e5
Tingting Duan <duantingting978@gmail.com>
2022-02-15 21:20:58 -0500

    _

--- sales_agent_total_sales.sql
+++ sales_agent_total_sales.sql
@@ -1,7 +1,7 @@
 select
     e.firstname,
     e.lastname,
-    sum(i.Total) as total_sales
+    sum(i.Total) as total_sales_per_employee
 from Customer c
     left JOIN Invoice i on c.customerid = i.customerid
     left JOIN Employee e on c.SupportRepId = e.EmployeeId
